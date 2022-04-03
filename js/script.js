8c461b2c5b325743c0ac4323edf060f8b57fe477
Roman <snapko0063@gmail.com>
2022-04-03 21:41:34 +0300

    almost done desc

--- js/script.js
+++ js/script.js
@@ -26,6 +26,131 @@ $(document).ready(function() {
             },
         ]
     });
-
+    $('.blogpop_card_slider').slick({
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        centerMode: true,
+        centerPadding: "200px"
+    });
 
 });
+
+let block = document.querySelectorAll(".choice_block");
+
+block.forEach((block) => {
+    function resizer1() {
+        this.classList.add("choice_block_card_big");
+        block.children[1].classList.remove("choice_block_card_big")
+    }
+
+    function resizer2() {
+        this.classList.remove("choice_block_card_big");
+        block.children[1].classList.add("choice_block_card_big")
+    }
+    block.children[0].addEventListener("mouseover", resizer1);
+    block.children[2].addEventListener("mouseover", resizer1);
+    block.children[0].addEventListener("mouseleave", resizer2);
+    block.children[2].addEventListener("mouseleave", resizer2);
+})
+
+
+
+
+
+
+
+
+let wedding = document.querySelector(".choice_wedding");
+let corp = document.querySelector(".choice_corp");
+let bth = document.querySelector(".choice_bth");
+
+corp.remove();
+bth.remove();
+
+function choose1() {
+    wedding.remove();
+    corp.remove();
+    bth.remove();
+    document.querySelector(".justPoint").append(wedding);
+    document.querySelector(".choice_switcher_wedding").style.backgroundColor = "#DB3130";
+    document.querySelector(".choice_switcher_corp").style.backgroundColor = "rgba(0,0,0,0)";
+    document.querySelector(".choice_switcher_bth").style.backgroundColor = "rgba(0,0,0,0)";
+}
+
+function choose2() {
+    wedding.remove();
+    corp.remove();
+    bth.remove();
+    document.querySelector(".justPoint").append(corp);
+    document.querySelector(".choice_switcher_corp").style.backgroundColor = "#DB3130";
+    document.querySelector(".choice_switcher_wedding").style.backgroundColor = "rgba(0,0,0,0)";
+    document.querySelector(".choice_switcher_bth").style.backgroundColor = "rgba(0,0,0,0)";
+}
+
+function choose3() {
+    wedding.remove();
+    corp.remove();
+    bth.remove();
+    document.querySelector(".justPoint").append(bth);
+    document.querySelector(".choice_switcher_bth").style.backgroundColor = "#DB3130";
+    document.querySelector(".choice_switcher_corp").style.backgroundColor = "rgba(0,0,0,0)";
+    document.querySelector(".choice_switcher_wedding").style.backgroundColor = "rgba(0,0,0,0)";
+}
+
+function goToWedding() {
+    document.querySelector(".choice").scrollIntoView(true);
+    choose1();
+
+}
+
+function goToCorp() {
+    document.querySelector(".choice").scrollIntoView(true);
+    choose2();
+}
+
+function goToBth() {
+    document.querySelector(".choice").scrollIntoView(true);
+    choose3();
+}
+
+$("body").on('click', '[href*="#"]', function(e) {
+    var fixed_offset = 20;
+    $('html,body').stop().animate({ scrollTop: $(this.hash).offset().top - fixed_offset }, 1000);
+    e.preventDefault();
+});
+$(window).on("scroll", function() {
+    $(".menu").toggleClass("active", $(this).scrollTop() > 10);
+    $(".menu_list_item_first").toggleClass("first", $(this).scrollTop() > 10);
+    $(".menu_list_item_last").toggleClass("last", $(this).scrollTop() > 10);
+    // document.querySelector(".menu_list_item_last").style.marginLeft = "0px";
+});
+document.querySelectorAll(".open").forEach((elem) => {
+    elem.onclick = function open() {
+        document.querySelector(".toDialog").style.left = "0";
+        return false;
+    }
+})
+document.querySelector(".toDialog_card_close").onclick = function close() {
+    document.querySelector(".toDialog").style.left = "100vw";
+    return false;
+}
+
+$(function() {
+    //задание заполнителя с помощью параметра placeholder
+    $(".date").mask("99.99.9999", { placeholder: "дд.мм.гггг" });
+    //задание заполнителя с помощью параметра placeholder
+    $(".phone").mask("+375 (99) 999-99-99", { placeholder: " " });
+});
+
+document.querySelector(".blogpop1_card_close").onclick = function close() {
+    document.querySelector(".blogpop1").style.left = "100vw";
+    return false;
+}
+document.querySelector(".blog_card1").onclick = function open() {
+    document.querySelector(".blogpop1").style.left = "0";
+    return false;
+}
---
+++ telegram.php
@@ -0,0 +1,44 @@
+
+<?php
+
+
+//В переменную $token нужно вставить токен, который нам прислал @botFather
+$token = "1937965576:AAEGSiV33Iw_rWyim2TylnIeOrPr5kLlKso";
+
+//Сюда вставляем chat_id
+$chat_id = "-1001529004217";
+
+//Определяем переменные для передачи данных из нашей формы
+if ($_POST['act'] == 'order') {
+  $name = $_POST['user_name'];
+  $phone = $_POST['user_phone'];
+  $data = $_POST['user_date'];
+}
+
+//Собираем в массив то, что будет передаваться боту
+    $arr = array(
+        'Имя:' => $_POST['user_name'],
+        'Телефон:' => $_POST['user_phone'],
+        'Дата:' => $_POST['user_date'],
+    );
+
+//Настраиваем внешний вид сообщения в телеграме
+    foreach($arr as $key => $value) {
+        $txt .= "<b>".$key."</b> ".$value."%0A";
+    };
+
+//Передаем данные боту
+    $sendToTelegram = fopen("https://api.telegram.org/bot{$token}/sendMessage?chat_id={$chat_id}&parse_mode=html&text={$txt}","r");
+
+//Выводим сообщение об успешной отправке
+    if ($sendToTelegram) {
+        echo('Success');
+    }
+
+//А здесь сообщение об ошибке при отправке
+    else {
+        echo('Error, try again later');
+    }
+
+
+?>
